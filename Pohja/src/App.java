7c8dde55762c13c42d6737ca876f55a8bd741a70
Jogis <ronipihajoki@proton.me>
2025-12-13 17:38:31 +0200

    0.1
    Added Examples of:
    > Class
    > Atrributes
    > Methods for Boolean

---
+++ Pohja/src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+} // App Ends
+
+class NameOfClass { // add class name here
+    
+        // We define attributes, we wish our object to have.
+        boolean isItOn;
+        String text;
+        int number;
+        double decimal;
+
+        // 1. We make a method. First declare is it Public or Private.
+        // 2. Then we can declare what it returns. If we return nothing we use void.
+        // 3. Then last we have method name that ends with. ()
+        /*1.*/public /*2.*/ void /*3.*/ methodName(){ // Boolean false method
+            isItOn = false;
+            System.out.println("We make this false. What ever it is.");
+        }
+
+        public void alsoMethodName(){ // Boolean true method
+            isItOn = true;
+            System.out.println("We make this true. Still dont know what it is.");
+        }
+
+
+
+
+    }
